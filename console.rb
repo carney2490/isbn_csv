aaa57b67ecf59cc9ccca148ec84658b901c09ae0
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-24 16:52:28 -0400

    updated

---
+++ console.rb
@@ -0,0 +1,3 @@
+require_relative "isbn.rb"
+
+user_isbn()
--- isbn.rb
+++ isbn.rb
@@ -1,40 +1,59 @@
-def get_isbn_array_count(isbn_number)
-	isbn_array = isbn_number.split("") {
+def get_isbn_array_count(user_isbn)
+	isbn_array = user_isbn.split("") {
 		|s| s.to_i
 
 	}
 	isbn_array.count
 end
 
-def string_length?(input_num)
-	if input_num.length == 10
+def string_length?(user_isbn)
+	if user_isbn.length == 10
 		true
 	else
 		false
 	end
 end
 
-def run_program()
-	if string_length? == true
-		figure_validity()
+def run_program(user_isbn)
+	if string_length?(user_isbn) == true
+		figure_validity(user_isbn)
+		
 	else
 		p "Must be 10 digits"
 	end
 end
 
-def figure_validity(isbn_number)
-	isbn_number = isbn_number.split("") {
+def figure_validity(user_isbn)
+	isbn_number = user_isbn.split("") {
 		|s| s.to_i
 	}
+	isbn_length = isbn_number.count
+	counter = 0
+	multiple = 10
+	sum = 0
 
-	isbn_sum = (isbn_number[0].to_i*10)+(isbn_number[1].to_i*9)+(isbn_number[2].to_i*8)+(isbn_number[3].to_i*7)+(isbn_number[4].to_i*6)+(isbn_number[5].to_i*5)+(isbn_number[6].to_i*4)+(isbn_number[7].to_i*3)+(isbn_number[8]).to_i*2)+isbn_number.fetch[9].to_i
+	isbn_length.times do 
+		sum = sum + (isbn_number[counter].to_i * multiple)
+		counter = counter + 1
+		multiple = multiple - 1
+	end
 
-	p isbn_sum 
+	# (isbn_number[0].to_i*10)+(isbn_number[1].to_i*9)+(isbn_number[2].to_i*8)+(isbn_number[3].to_i*7)+(isbn_number[4].to_i*6)+(isbn_number[5].to_i*5)+(isbn_number[6].to_i*4)+(isbn_number[7].to_i*3)+(isbn_number[8]).to_i*2)+isbn_number.fetch[9].to_i
 
-	if isbn_sum % 11 == 0
+	if sum % 11 == 0
+		p "Congrats your ISBN number is valid"
 		true
+
 	else
+		p "Your isbn is invalid"
 		false
 	end
+
+end
+
+def user_isbn()
+puts "What is your isbn number?"
+user_isbn = gets.chomp
+run_program(user_isbn)
 
 end
