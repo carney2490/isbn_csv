9e2485576594ac5ba3fe354fdb68f38255011b8d
Trixie Devine <sward@Stacy-Wards-MacBook-Pro-3.local>
2017-07-24 15:50:18 -0400

    fixed array syntax

--- isbn.rb
+++ isbn.rb
@@ -27,7 +27,7 @@ def figure_validity(isbn_number)
 		|s| s.to_i
 	}
 
-	isbn_sum = (isbn_number.fetch(0).to_i*10)+(isbn_number.fetch(1).to_i*9)+(isbn_number.fetch(2).to_i*8)+(isbn_number.fetch(3).to_i*7)+(isbn_number.fetch(4).to_i*6)+(isbn_number.fetch(5).to_i*5)+(isbn_number.fetch(6).to_i*4)+(isbn_number.fetch(7).to_i*3)+(isbn_number.fetch(8).to_i*2)+isbn_number.fetch(9).to_i
+	isbn_sum = (isbn_number[0].to_i*10)+(isbn_number[1].to_i*9)+(isbn_number[2].to_i*8)+(isbn_number[3].to_i*7)+(isbn_number[4].to_i*6)+(isbn_number[5].to_i*5)+(isbn_number[6].to_i*4)+(isbn_number[7].to_i*3)+(isbn_number[8]).to_i*2)+isbn_number.fetch[9].to_i
 
 	p isbn_sum 
 
